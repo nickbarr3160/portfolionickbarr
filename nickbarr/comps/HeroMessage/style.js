54945a97731e893f14c5c21d70f2f5da4bf3abaf
nickbarr3160 <72280263+nickbarr3160@users.noreply.github.com>
2023-09-25 15:51:09 -0700

    adjust breakpoints, increase heroMessage name font size

--- nickbarr/comps/HeroMessage/style.js
+++ nickbarr/comps/HeroMessage/style.js
@@ -10,7 +10,7 @@ width:100%;
 height:50vh;
 margin:1em;
 margin:20em 0 0em 0;
-@media only screen and (max-width: 950px) {
+@media only screen and (max-width: 1100px) {
     flex-direction:column;
 }
 `;
@@ -21,17 +21,19 @@ flex-direction:column;
 justify-content:center;
 align-items:center;
 width:50%;
-height:100%;
+height:385px;
 color:${props=>props.color};
 @media only screen and (max-width: 600px) {
     width:80%;
     align-items:flex-start;
 }
-@media only screen and (min-width: 600px) and (max-width: 950px) {
+@media only screen and (min-width: 600px) and (max-width: 1100px) {
     justify-content:flex-start;
     height:auto;
     flex-direction:row;
     width:90%;
+}
+margin-right:15px;
 }   
 `;
 
@@ -47,13 +49,13 @@ justify-content:flex-start;
 text-align:left;
 flex-wrap:wrap;
 font-weight:bold;
-font-size:4em;
+font-size:5em;
 animation: 1.5s ${fadeAnimation};
 @media only screen and (max-width: 900px) {
     width:90%;
     font-size:3em;
 } 
-@media only screen and (min-width: 600px) and (max-width: 950px) {
+@media only screen and (min-width: 600px) and (max-width: 1100px) {
     font-size:4em;
     width:auto;
     margin-right:15px;
@@ -63,11 +65,11 @@ animation: 1.5s ${fadeAnimation};
 export const IntroDescrip = styled.div`
 display:flex;
 flex-direction:column;
-justify-content:center;
 align-items:center;
+justify-content:space-evenly;
 width:50%;
+height:385px;
 color:${props=>props.color};
-height:100%;
 padding-right:1em;
 @media only screen and (max-width: 600px) {
     width:80%;
@@ -76,11 +78,12 @@ padding-right:1em;
 @media only screen and (min-width: 1300px) {
     font-size:25px;
 }
-@media only screen and (min-width: 600px) and (max-width: 950px) {
+@media only screen and (min-width: 600px) and (max-width: 1100px) {
     width:90%;
+    font-size:20px;
     justify-content:center;
-    height:60%;
-} 
+    height:50%;
+}  
 `;
 
 export const ButtonCont = styled.div`
