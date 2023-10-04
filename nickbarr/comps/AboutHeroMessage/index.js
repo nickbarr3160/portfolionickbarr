1f4fb1754294ee4bd9de2b20bc8da3068055ecee
nickbarr3160 <72280263+nickbarr3160@users.noreply.github.com>
2023-10-04 13:31:58 -0700

    add link to blog on about page

--- nickbarr/comps/AboutHeroMessage/index.js
+++ nickbarr/comps/AboutHeroMessage/index.js
@@ -9,7 +9,9 @@ const AboutIntro = ({
     introText="Hey, my name is",
     name="Nicholas Barr",
     phrase="Get to know me",
-    introPar="I am a Front-End Developer with a focus on crafting exceptional digital experiences. Beyond development, I also dabble in creating striking graphics and visuals, blending technical proficiency with aesthetic intuition to bring ideas to life."
+    introPar="I am a Front-End Developer with a focus on crafting exceptional digital experiences. Beyond development, I also dabble in creating striking graphics and visuals, blending technical proficiency with aesthetic intuition to bring ideas to life. Check out a ",
+    blogLink="blog",
+    postText=" I wrote about my first year in the industry !"
 }) => {
     const {theme} = useTheme();
     var buttonData=ButtonData
@@ -17,7 +19,18 @@ const AboutIntro = ({
         <IntroText  color={About_Intro_theme[theme].heading1Col} >{introText}</IntroText>
         <HeadingCont color={About_Intro_theme[theme].heading2Col}>{name}</HeadingCont>
         <HeadingCont color={About_Intro_theme[theme].heading3Col}>{phrase}</HeadingCont>
-        <IntroPar color={About_Intro_theme[theme].parCol}>{introPar}</IntroPar>
+        <IntroPar color={About_Intro_theme[theme].parCol}>
+            {introPar}
+            <a 
+                href="https://mellenger.com/blog/ready-player-one-sharing-best-cheat-codes-my-first-year-front-end-development" 
+                style={{ color:About_Intro_theme[theme].heading1Col, textDecoration: 'underline' }}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {blogLink}
+            </a>
+            {postText}
+        </IntroPar>
         <ButtonCont>
                     {buttonData.map((o,i)=>{
                     return <Button
