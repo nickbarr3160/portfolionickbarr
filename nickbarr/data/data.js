5c0381e56887d4d1a8433b042aaaa6b9b3c40166
nickbarr3160 <72280263+nickbarr3160@users.noreply.github.com>
2023-10-26 10:58:45 -0700

    re update resume due to grammar error

--- nickbarr/data/data.js
+++ nickbarr/data/data.js
@@ -178,7 +178,7 @@ export const ButtonData = [
     {
         label:"Resume",
         icon:<AiFillFilePdf/>,
-        route:'/NicholasSameerBarrTechResume.PDF'
+        route:'/NicholasSameerBarrTechResume.pdf'
     },
 ]
 
